001e1a968505b20967c1b628531c79402fd68ce0
KSU\ksusm <xkuznet4@mendelu.cz>
2025-12-09 21:37:40 +0100

    question service changed error to use ApiError

--- backend/src/services/question.service.ts
+++ backend/src/services/question.service.ts
@@ -2,17 +2,19 @@ import Question from "../database/models/question.model";
 import mongo from "../database/mongo";
 import { ObjectId } from "mongodb";
 import { QuestionDto } from "../types/dto/question.dto";
+import { ApiError } from "../types/api.error";
 
 const questionService = {
   get question_collection() {
-    if (!mongo.db) throw new Error("Mongo not connected");
+    if (!mongo.db) throw new ApiError('database error', 'Mongo not connected', 500);
     return mongo.db.collection("questions");
   },
 
   async create(questionDto: QuestionDto) {
     if (questionDto.correctIndex >= questionDto.answers.length) {
-      throw new Error("correctIndex is out of bounds");
+      throw new ApiError('bad request', 'correctIndex is out of bounds', 400);
     }
+
     const question = new Question(
         questionDto.text,
         questionDto.answers,
@@ -27,18 +29,23 @@ const questionService = {
   },
 
   async findById(id: string) {
-    const question = await this.question_collection.findOne({ _id: new ObjectId(id) });
+    const question = await this.question_collection.findOne({
+      _id: new ObjectId(id)
+    });
     return question || null;
   },
 
   async update(id: string, questionDto: QuestionDto) {
-    const existingQuestion = await this.question_collection.findOne({ _id: new ObjectId(id) });
+    const existingQuestion = await this.question_collection.findOne({
+      _id: new ObjectId(id)
+    });
+
     if (!existingQuestion) {
       return null;
     }
 
     if (questionDto.correctIndex >= questionDto.answers.length) {
-      throw new Error("correctIndex is out of bounds");
+      throw new ApiError('bad request', 'correctIndex is out of bounds', 400);
     }
 
     const result = await this.question_collection.findOneAndUpdate(
